f8a2a004f9a56de970b2f8f040664bef74e2e343
Regina1997 <56130620+Regina1997@users.noreply.github.com>
2023-10-10 13:25:05 +0300

    export changes

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,2 +1,2 @@
-export * as UserController from './UserController.js';
-export * as PostController from './PostController.js';
+export { default as UserController } from './UserController.js';
+export { default as PostController } from './PostController.js';
